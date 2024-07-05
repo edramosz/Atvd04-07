542518537b1d898ab2c894305cbf2c871bc39ea7
edramosz <163909955+edramosz@users.noreply.github.com>
2024-07-05 16:44:22 -0300

    ajuste

--- Atvd/Program.cs
+++ Atvd/Program.cs
@@ -6,5 +6,10 @@ Exer2 ex2 = new Exer2();
 //ex2.ConversaoDolar(56);
 
 Exer3 ex3 = new Exer3();
-ex3.CalcularComissao();
+//ex3.CalcularComissao();
 
+Exer4 ex4 = new Exer4();
+//ex4.TrocarValores();
+
+Exer5 ex5 = new Exer5();
+ex5.Modulo();
